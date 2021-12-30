9155a7a2d913f8380e1ca61541cf606c5d20c236
Takuma.S <baaninreo@gmail.com>
2021-12-30 10:26:26 +0900

    初期環境構築完了

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    //"es2021": true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "jest"],
+  rules: {},
+  settings: {
+    "import/resolver": {
+      node: {
+        moduleDirectory: ["node_modules", "/"],
+      },
+    },
+  },
+};
