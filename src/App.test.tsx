19a3fc6e2422c3c494c6c458f4b181fa7307affd
Takuma.S <baaninreo@gmail.com>
2022-02-25 16:06:01 +0900

    ダミーテストケースを追加

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+describe("Dummy test case", () => {
+  it("Dummy 1", async () => {
+    const dummy_text = "This is dummy test case";
+    expect(dummy_text).toEqual("This is dummy test case");
+  });
+});
+
+export {}
